611d2d04df2232649b21b27bb2812b8cfe391a33
Cristian <thereminchile@gmail.com>
2025-01-22 01:00:44 -0300

    menos delay

--- src/main.cpp
+++ src/main.cpp
@@ -25,5 +25,5 @@ void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(10);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  delay(10);               // wait for a second
 }
